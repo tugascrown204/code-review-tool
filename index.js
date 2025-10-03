fca4412bd3dc379abe889032ffea4b5daa6753c5
tugascrown204 <gajdertevet@hotmail.com>
2025-10-03 19:58:47 +0330

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/codeReviewTool', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the Code Review Tool!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
